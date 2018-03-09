bd8cf0e1433108efe6213c651b340ebc0f4cb505
John Torres <john.torres.hurtado@gmail.com>
2018-03-09 09:50:10 -0800

    last changes added price

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const tiers = [
     protection: 10,
     vpn: 'VPN',
     downtime: 1,
-    failover: 'and a maximum of 8hrs of failover every quarter'
+    failover: 'and a maximum of 8 hours of failover every quarter'
   },
   {
     tier: 'Business',
@@ -15,7 +15,7 @@ const tiers = [
     protection: 50,
     vpn: 'VPN + 2 public IPs',
     downtime: 5,
-    failover: 'and a maximum of 8hrs of failover every quarter'
+    failover: 'and a maximum of 8 hours of failover every quarter'
   },
   {
     tier: 'Enterprise',
@@ -24,128 +24,9 @@ const tiers = [
     protection: 100,
     vpn: 'Direct Connect + 5 Public IPs',
     downtime: 5,
-    failover: 'and a maximum of 8hrs of failover every quarter, 3 days of Downtime in a year.'
+    failover: 'and a maximum of 8 hours of failover every quarter, 3 days of Downtime in a year.'
   }
 ];
-
-(function() {
-    var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
-
-    NUM_CONFETTI = 80;
-
-    COLORS = [[219, 69, 59], [51, 42, 134], [94, 151, 50], [227, 111, 30], [138, 31, 3], [255, 201, 7], [0, 165, 227], [16, 69, 89]];
-
-    PI_2 = 2 * Math.PI;
-
-    canvas = document.getElementById("confetti");
-
-    context = canvas.getContext("2d");
-
-    window.w = 0;
-
-    window.h = 0;
-
-    resizeWindow = function() {
-      window.w = canvas.width = window.innerWidth;
-      return window.h = canvas.height = window.innerHeight;
-    };
-
-    window.addEventListener('resize', resizeWindow, false);
-
-    window.onload = function() {
-      return setTimeout(resizeWindow, 0);
-    };
-
-    range = function(a, b) {
-      return (b - a) * Math.random() + a;
-    };
-
-    drawCircle = function(x, y, r, style) {
-      context.beginPath();
-      context.arc(x, y, r, 0, PI_2, false);
-      context.fillStyle = style;
-      return context.fill();
-    };
-
-    xpos = 0.5;
-
-    document.onmousemove = function(e) {
-      return xpos = e.pageX / w;
-    };
-
-    window.requestAnimationFrame = (function() {
-      return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
-          return window.setTimeout(callback, 1000 / 60);
-        };
-    })();
-
-    Confetti = (function() {
-      function Confetti() {
-        this.style = COLORS[~~range(0, COLORS.length)];
-        this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
-        this.r = ~~range(2, 6);
-        this.r2 = 2 * this.r;
-        this.replace();
-      }
-
-      Confetti.prototype.replace = function() {
-        this.opacity = 0;
-        this.dop = 0.02 * range(1, 4);
-        this.x = range(-this.r2, w - this.r2);
-        this.y = range(-20, h - this.r2);
-        this.xmax = w - this.r;
-        this.ymax = h - this.r;
-        this.vx = range(0, 2) + 8 * xpos - 5;
-        return this.vy = 0.7 * this.r + range(-1, 1);
-      };
-
-      Confetti.prototype.draw = function() {
-        var ref;
-        this.x += this.vx;
-        this.y += this.vy;
-        this.opacity += this.dop;
-        if (this.opacity > 1) {
-          this.opacity = 1;
-          this.dop *= -1;
-        }
-        if (this.opacity < 0 || this.y > this.ymax) {
-          this.replace();
-        }
-        if (!((0 < (ref = this.x) && ref < this.xmax))) {
-          this.x = (this.x + this.xmax) % this.xmax;
-        }
-        return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
-      };
-
-      return Confetti;
-
-    })();
-
-    confetti = (function() {
-      var j, ref, results;
-      results = [];
-      for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
-        results.push(new Confetti);
-      }
-      return results;
-    })();
-
-    window.step = function() {
-      var c, j, len, results;
-      requestAnimationFrame(step);
-      context.clearRect(0, 0, w, h);
-      results = [];
-      for (j = 0, len = confetti.length; j < len; j++) {
-        c = confetti[j];
-        results.push(c.draw());
-      }
-      return results;
-    };
-
-    step();
-
-}).call(this);
-
 
 $(document).ready(function(){
 
@@ -168,46 +49,111 @@ $(document).ready(function(){
       </div>
       `
   ));
-  $('.primary').one('click',function(){
+  $('.primary').one('click', () => {
+    $('.container').css('justify-content','flex-start');
     $('.details').hide().append(`
         <h2> Plan Details </h2>
         <div class='table-wrapper'>
           <table>
             <tr>
-              <th>Product</th>
-              <th>Item</th>
+              <th>Disaster Recovery</th>
+              <th style='text-align: right;'>Quantity</th>
               <th>Action</th>
             </tr>
             <tr>
               <td><input type='checkbox' checked/>  Protected VMs</td>
-              <td>3</td>
+              <td style='text-align: right;'>3</td>
               <td class='right action'>Edit</td>
             </tr>
             <tr>
-              <td><input type='checkbox' checked/>  24/7 VMs</td>
-              <td>2</td>
+              <td><input type='checkbox' checked/>  CPU per VM (GHz)</td>
+              <td style='text-align: right;'> 80</td>
               <td class='right action' >Edit </td>
             </tr>
             <tr>
-              <td><input type='checkbox' checked/>  Connections</td>
-              <td> VPN </td>
+              <td><input type='checkbox' checked/>  Memory per VM (TiB)</td>
+              <td style='text-align: right;'> 90  </td>
               <td class='right action'> Edit </td>
             </tr>
+
             <tr>
-              <td><input type='checkbox' checked/>  Failover</td>
-              <td> 8hrs every quarter </td>
+              <td><input type='checkbox' checked/>Recovery Level</td>
+              <td style='text-align: right;'> Gold</td>
               <td class=' right action'> Edit </td>
             </tr>
           </table>
         </div>
+
+        <div class='table-wrapper'>
+          <table>
+            <tr>
+              <th>Networking</th>
+              <th style='text-align: right;'>Quantity</th>
+              <th>Action</th>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/>  Public IP Count</td>
+              <td style='text-align: right;'>30</td>
+              <td class='right action'>Edit</td>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/>  Connection type</td>
+              <td style='text-align: right;'>VPN</td>
+              <td class='right action' >Edit </td>
+            </tr>
+          </table>
+        </div>
+
+        <div class='table-wrapper'>
+          <table>
+            <tr>
+              <th>Virtual Machines as a service</th>
+              <th style='text-align: right;'>Quantity</th>
+              <th>Action</th>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/>  Running VMs</td>
+              <td style='text-align: right;'>1000</td>
+              <td class='right action'>Edit</td>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/>  CPU per VM (GHz)</td>
+              <td style='text-align: right;'> 80</td>
+              <td class='right action' >Edit </td>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/>  Memory per VM (TiB)</td>
+              <td style='text-align: right;'> 90  </td>
+              <td class='right action'> Edit </td>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/> HDD per VM (TiB)</td>
+              <td style='text-align: right;'> 400 </td>
+              <td class=' right action'> Edit </td>
+            </tr>
+            <tr>
+              <td><input type='checkbox' checked/> Flash per VM (TiB)</td>
+              <td style='text-align: right;'> 80 </td>
+              <td class=' right action'> Edit </td>
+            </tr>
+          </table>
+        </div>
+        <h2 style='margin-top:30px;'> Payment details</h2>
+        <div class='price'>
+          <h1>$ 20102.00</h1>
+          <h3> Pay as you go </h3>
+        </div>
         `
-    ).slideDown(600, function(){
+    ).slideDown( 600, () => {
       $('.details').addClass('visible');
+
     });
 
-    $('.container').css('justify-content','flex-start');
+
+    setTimeout(function() {
+      $('body').animate({ scrollTop: $(document).height() }, 2400);
+    }, 800);
+
 
   });
-
-
 });
