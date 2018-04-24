2a8f17b57dc96b1f39c63e96e29cb898bf2e5711
john <john.torres.hurtado@gmail.com>
2018-04-24 11:15:46 -0400

    added 2nd page info
    added second page information and logic.

--- js/data.js
+++ js/data.js
@@ -1,3 +1,39 @@
+const blank =`<h2> Welcome to Xi Pricer </h2>
+  <h3 class='alt' style='margin-top:10px 0 0 0;'>Select any configuration to begin</h3>
+  <hr>
+
+  <form>
+    <h3> <input type="radio" id='config' name='select' value='0'/> Small environment</h3>
+    <p class='alignment alt'>10 protected Virtual Machines, VPN and 1 limited time test failover every quarter</p>
+
+    <h3> <input type="radio" id='config' name='select' value='1'/> Medium environment (Mid-market customer) </h3>
+    <p class='alignment alt'>50 protected Virtual Machines, 2 VMs running 24X7, VPN, 2 Public IPs and 1 test failover per quarter</p>
+
+    <h3> <input type="radio" id='config' name='select' value='2'/> Large environment (Enterprise customer)</h3>
+    <p class='alignment alt'>200 Protected Virtual Machines with 20 mission-critical VMs, 10 VMs running 24X7, Direct Connect, 10 Public IPs, and long running test failovers every quarter </p>
+
+    <h3> <input type="radio" id='config' name='select' value='3'/> Custom Configuration</h3>
+    <p class='alignment alt'>Select your own options and configurations</p>
+  </form>
+  <hr>
+
+  <button class='primary fw' id='continue'> Continue </button>
+`
+
+const small =
+  `<h1> Xi Pricer 0</h1>`;
+
+const medium =
+  `<h1> Xi Pricer 1</h1>`;
+
+const large =
+  `<h1> Xi Pricer 2</h1>`;
+
+const custom =
+  `<h1> Xi Pricer 3</h1>`;
+
+
+
 const tiers = [
   {
     tier: 'Starter',
@@ -27,8 +63,6 @@ const tiers = [
     failover: 'and a maximum of 8 hours of failover every quarter, 3 days of Downtime in a year.'
   }
 ];
-
-
 const items = [
   { name: 'Disaster Recovery',
     elements:[
--- js/script.js
+++ js/script.js
@@ -1,90 +1,14 @@
+const selection = [small, medium, large, custom, blank];
 
 $(document).ready(function(){
+  // $('#selection').html(selection[4]);
+  $('#continue').click(()=> {
 
-  $('.deck').append(tiers.map(tier =>
-    `
-      <div class="card">
-        <div class="card-header">
-          <h1>${tier.tier}</h1>
-          <p>${tier.subtitle}</p>
-        </div>
-        <div class="card-body">
-          <p> ${tier.protection} Protected Virtual Machines, ${tier.downtime} Virtual Machine without downtime, ${tier.vpn} ${tier.failover}</p>
-        </div>
-        <div class="card-footer">
-          <h2>${tier.price}</h2>
-          <p>Some text here</p>
-          <button class='primary'>Choose plan</button>
-        </div>
-      </div>
-    `
-  ));
-
-  $('.header').after(
-    `<div class='sub-header'>
-      <div class='content'>
-        <h2> Starter Package</h2>
-        <div><h2 class='total-amount' style='line-height:0'> $12000.00 <span>/mo</span></h2><a style='line-height:1.5' id='payment'>edit payment plan</a></div>
-      </div>
-    </div>`
+    if($('input:checked').val() == 0){$('#selection').html(selection[0])}
+    if($('input:checked').val() == 1){$('#selection').html(selection[1])}
+    if($('input:checked').val() == 2){$('#selection').html(selection[2])}
+    if($('input:checked').val() == 3){$('#selection').html(selection[3])}
+  }
   );
-
-  $('.primary').one('click', () => {
-    $('.container').css('justify-content','flex-start');
-    $('.details').hide().append(
-      `
-        ${items.map(item =>
-          `
-          <div class='table-title'>
-            <h3>${item.name}</h3>
-            <a class='${item.class}'>edit</a>
-          </div>
-
-          <div class='table-wrapper'>
-              <table>
-                <tr>
-                  <th>Products</th>
-                  <th style='text-align:right;'>Quantity</th>
-                  <th style='text-align: right;'>Price</th>
-                  <th style='text-align: right;'>Total</th>
-                </tr>
-                ${item.elements.map(element =>
-                  `<tr>
-                    <td>${element.title}</td>
-                    <td style='text-align: right;'>${element.quantity}</td>
-                    <td sytle='text-align: right;' class='right'>$${element.price}.00</td>
-                    <td sytle='text-align: right;' class='right totalprice'>$${isNaN(parseInt(element.price * element.quantity))? element.price : element.price * element.quantity }.00</td>
-                  </tr>`
-                ).join('')}
-              </table>
-          </div>
-        `).join('')}`
-    ).slideDown(550,() => $('.details').addClass('visible'));
-
-    $('.rec').click(()=> $('body').append(CreatePopup(1,1,1)))
-    $('.net').click(()=> $('body').append(CreatePopup(2,2,1)))
-    $('.vmaas').click(()=> $('body').append(CreatePopup(3,3,1)))
-    $('#payment').click(() => $('body').append(CreatePopup(4,4,2)))
-
-    var sum = 0;
-
-    $(".totalprice").each(function() {
-      var val = $.trim($(this).text());
-      if (val) {
-          val = parseFloat( val.replace( /^\$/, "" ) );
-          sum += !isNaN( val ) ? val : 0;
-      }
-    });
-
-    $('.total-amount').text('$'+ sum + '.00').append(`<span>/mo</span>`);
-
-    setTimeout(() => {
-      $('.sub-header').addClass('reveal');
-
-      $('body').animate({scrollTop: $(document).height()}, 2400
-    )},500
-
-    )
-  });
 
 });
